8f12e06cfe2d2bbc9afa7d1f41a176788ab0504f
annekuo <59021059+annekuo@users.noreply.github.com>
2023-05-18 14:28:22 -0400

    Updated result message

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function calculate() {
     var results = food * r;
     results = results.toFixed(2);
   
-    document.getElementById("results").innerHTML = `<article class="message is-link"><div class="message-body">You should be paying <span class="has-text-weight-bold">${results}</span> dollars.</div></article>`;
+    document.getElementById("results").innerHTML = `<article class="message is-link"><div class="message-body">You should pay <span class="has-text-weight-bold">${results}</span> dollars.</div></article>`;
   }
   
